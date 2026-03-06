70c4f4d2d08d28ce59aa7d14b057778870ee3e11
Dilip Kumar <dilip.kumar@kohler.com>
2026-03-07 01:09:30 +0530

    Added Shop page

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -4,6 +4,7 @@ export default function Navbar() {
   return (
     <div className="flex gap-4">
       <Links href="/" text="Home" underline={false} />
+      <Links href="/shop" text="Shop" underline={false} />
       <Links href="/about" text="About" underline={false} />
       <Links href="/contact" text="Contact" underline={false} />
     </div>
